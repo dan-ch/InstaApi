5902266272703e8b5b37553e84f9cc4f95096b10
dan-ch <daniel666ch@gmail.com>
2021-12-03 14:49:45 +0100

    refactor: authController and postController refactor, organize api routes to be more readable

--- routes/api.php
+++ routes/api.php
@@ -1,63 +1,38 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\CommentController;
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
-
-Route::middleware('auth:sanctum')->get('/test', function (){
-    return response()->json('Protected route', 201);
-});
+use App\Http\Controllers\{
+    AuthController,
+    CommentController,
+    UserController,
+    PostController,
+};
 
 
 // Posts routes
-Route::group([
-    'prefix' => '/posts',
-    'as' => 'posts.',
-    ], function (){
-
-    Route::get('/', [PostController::class, 'index'])
-    ->name('all');
-
-    Route::get('/{postId}', [PostController::class, 'show']);
-
-    Route::post('/', [PostController::class, 'store']);
-
-    Route::delete('/{postId}', [PostController::class, 'destroy']);
-
-    Route::match(['patch', 'put'], '/{postId}', [PostController::class, 'update']);
-
+Route::middleware('auth:sanctum')->group(function (){
+    Route::apiResource('/posts', PostController::class)->withoutMiddleware('auth:sanctum');
 });
 
+
 // Comments routes
-
-Route::apiResource('/comments', CommentController::class);
-
+Route::middleware('auth:sanctum')->group(function (){
+    Route::apiResource('/comments', CommentController::class);
+});
 
 // Users routes
-Route::apiResource('/users', UserController::class);
+Route::middleware('auth:sanctum')->group(function (){
+    Route::apiResource('/users', UserController::class);
+    Route::get('/users/{user}/posts', [UserController::class, 'posts']);
+});
 
 
 // Auth routes
+Route::group(['prefix' => '/login'], function (){
+    Route::post('', [AuthController::class, 'login']);
+    Route::get('/{provider}', [AuthController::class, 'redirectToProvider']);
+    Route::get('/{provider}/callback', [AuthController::class, 'providerCallback']);
+    Route::post('/password-reset', [AuthController::class, 'passwordReset']);
+});
 Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
 Route::middleware(['auth:sanctum'])->post('/logout', [AuthController::class, 'logout']);
-
-Route::get('/login/{provider}', [AuthController::class, 'redirectToProvider']);
-Route::get('/login/{provider}/callback', [AuthController::class, 'providerCallback']);
